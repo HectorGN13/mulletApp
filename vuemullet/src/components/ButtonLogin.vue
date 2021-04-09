f6a39902359a14fef85af167d1ce6348d03036cc
Hector E. Garcia Nuñez <hectoreduardo.garcia@iesdonana.org>
2021-04-09 16:19:54 +0200

    ADD: Formulario de Login en ventana modal

--- vuemullet/src/components/ButtonLogin.vue
+++ vuemullet/src/components/ButtonLogin.vue
@@ -2,37 +2,122 @@
   <div class="text-center">
     <v-btn
         class="ma-2 special--text"
-        :loading="loading"
-        :disabled="loading"
+        :loading="visibility"
+        :disabled="visibility"
         color="#332D2E"
         backgroundColor="#FAF6E6"
-        @click="loader = 'loading'"
+        @click="visibility = true"
         x-large
     >
       LOG IN
     </v-btn>
+    <!-- Dialog -->
+    <v-dialog v-model="visibility" width="800px" persistent>
+      <v-card>
+        <v-card-title>Titulo </v-card-title>
+        <v-card-text>
+          <v-container>
+            <v-row>
+              <v-text-field
+                  v-model="name"
+                  :error-messages="nameErrors"
+                  :counter="10"
+                  label="Name"
+                  required
+                  @input="$v.name.$touch()"
+                  @blur="$v.name.$touch()"
+              ></v-text-field>
+            </v-row>
+            <v-row>
+              <v-text-field
+                  v-model="email"
+                  :error-messages="emailErrors"
+                  label="E-mail"
+                  required
+                  @input="$v.email.$touch()"
+                  @blur="$v.email.$touch()"
+              ></v-text-field>
+            </v-row>
+          <v-row>
+            <v-checkbox
+                v-model="checkbox"
+                :error-messages="checkboxErrors"
+                label="Do you agree?"
+                required
+                @change="$v.checkbox.$touch()"
+                @blur="$v.checkbox.$touch()"
+            ></v-checkbox>
+          </v-row>
+          </v-container>
+        </v-card-text>
+        <v-divider></v-divider>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn @click="visibility = false">CLOSE</v-btn>
+          <v-spacer></v-spacer>
+          <v-btn
+              class="mr-4"
+              @click="submit"
+          >
+            submit
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+    <!-- End Dialog -->
   </div>
 </template>
 
 <script>
+import { validationMixin } from 'vuelidate'
+import { required, maxLength, email } from 'vuelidate/lib/validators'
+
 export default {
   name: 'button_login',
-  data () {
-    return {
-      loader: null,
-      loading: false,
-    }
-  },
-  watch: {
-    loader () {
-      const l = this.loader
-      this[l] = !this[l]
+  mixins: [validationMixin],
 
-      setTimeout(() => (this[l] = false), 3000)
-
-      this.loader = null
+  validations: {
+    name: { required, maxLength: maxLength(10) },
+    email: { required, email },
+    checkbox: {
+      checked (val) {
+        return val
+      },
     },
   },
+
+  data () {
+    return {
+      visibility: false,
+      name: '',
+      email: '',
+      checkbox: false,
+    };
+  },
+
+  computed: {
+    nameErrors () {
+      const errors = []
+      if (!this.$v.name.$dirty) return errors
+      !this.$v.name.maxLength && errors.push('Name must be at most 10 characters long')
+      !this.$v.name.required && errors.push('Name is required.')
+      return errors
+    },
+    emailErrors () {
+      const errors = []
+      if (!this.$v.email.$dirty) return errors
+      !this.$v.email.email && errors.push('Must be valid e-mail')
+      !this.$v.email.required && errors.push('E-mail is required')
+      return errors
+    },
+  },
+
+  methods: {
+    submit () {
+      this.$v.$touch()
+    },
+  },
+
 }
 </script>
 
